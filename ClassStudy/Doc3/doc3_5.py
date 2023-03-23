464175888df0d96dd911aa7445069dd0c04367f9
dandan1232 <345652127@qq.com>
2023-03-23 14:44:23 +0800

    编写程序，完成500以内任意一个合数的质因数分解。

--- ClassStudy/Doc3/doc3_5.py
+++ ClassStudy/Doc3/doc3_5.py
@@ -1,7 +1,60 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/3/23 13:45
+# @Time    : 2023/3/23 14:08
 # @Author  : Lindand
 # @File    : doc3_5.py
 # @Description :
 # 编写程序，完成500以内任意一个合数的质因数分解。
 # 让用户输入一个500以内的合数，找出其所有质因数，并按照质因数乘法的形式输出，例如，60=2*2*3*5
+
+'''在这个程序中，我们使用了一个名为 fenjie 的函数来计算质因数。
+每次找到最小的质因数，将其添加到因子列表中，并将原始数除以该质因数，不断迭代直到原始数变为1为止。
+最后，将所有因子按照乘法的形式输出即可。'''
+
+
+def fenjie(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+'''在这个程序中，首先定义了一个名为 is_prime 的函数，用于检查一个数是否为质数。
+从2到该数的平方根之间的所有数进行遍历，并检查是否有数能够整除该数。
+如果存在这样的数，该数就不是质数。如果所有可能的数都不能整除该数，那么该数就是质数。'''
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+'''在程序的主体中，要求用户输入一个合数。
+如果用户输入的数不在500以内，或者不是一个合数（例如素数），则程序会输出一条错误消息并结束。
+否则，我们就调用 fenjie 函数来计算质因数，并将结果输出。'''
+
+num = int(input("请输入一个500以内的合数: "))
+
+if num < 1 or num > 500:
+    print("输入有误！该数超出范围啦！！")
+else:
+    if is_prime(num):
+            print("输入有误！该数为质数")
+    else:
+        factors = fenjie(num)
+        print(f"{num}=", end="")
+        for i, factor in enumerate(factors):
+            if i == len(factors) - 1:
+                print(factor)
+            else:
+                print(f"{factor}*", end="")
--- ClassStudy/Doc3/doc3_6.py
+++ ClassStudy/Doc3/doc3_6.py
@@ -1,7 +1,64 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/3/23 13:45
+# @Time    : 2023/3/23 14:19
 # @Author  : Lindand
 # @File    : doc3_6.py
 # @Description :
 # 随机生成一个长度是100的整数列表，每个元素是100以内的正整数，使用遍历方法求该列表中不同的质数，
 # 并求出该列表中有多少个质数可以表达为该列表中另外两个质数的和
+
+
+import random
+
+'''在这个程序中，首先定义了一个名为 is_prime 的函数，用于检查一个数是否为质数。
+从2到该数的平方根之间的所有数进行遍历，并检查是否有数能够整除该数。
+如果存在这样的数，该数就不是质数。如果所有可能的数都不能整除该数，那么该数就是质数。'''
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+'''接下来，定义了一个名为 find_prime_numbers 的函数，用于遍历输入的列表并找出其中的所有质数。
+这个函数利用了 is_prime 函数来判断每个数是否为质数，并使用一个集合来存储所有找到的质数。'''
+
+
+def find_prime_numbers(lst):
+    primes = set()
+    for num in lst:
+        if is_prime(num):
+            primes.add(num)
+    return primes
+
+
+'''然后，我们定义了一个名为 find_sum_of_primes 的函数，用于找出列表中可以表示为另外两个质数之和的质数的数量。
+首先，我们使用 find_prime_numbers 函数来找出列表中的所有质数。
+然后，我们遍历其中的每对质数，并检查它们的和是否也是一个质数。
+如果是，我们就增加计数器的值，表示找到了一个可以表示为另外两个质数之和的质数。
+需要注意的是，我们只需要找到一个与该质数的和相等的质数即可，因此我们使用了一个 break 语句来提前结束内层循环。'''
+
+
+def find_sum_of_primes(lst):
+    primes = find_prime_numbers(lst)
+    count = 0
+    for prime1 in primes:
+        for prime2 in primes:
+            if prime1 != prime2 and prime1 + prime2 in primes:
+                count += 1
+                break
+    return count
+
+
+'''最后，我们生成一个长度为100的随机整数列表，并分别调用 find_prime_numbers 和 find_sum_of_primes 函数来计算所需的结果。
+程序会输出生成的列表、不同的质数数量和可以表示为另外两个质数之'''
+
+lst = [random.randint(1, 100) for _ in range(100)]
+print(f"生成的随机整数列表为：{lst}")
+primes = find_prime_numbers(lst)
+print(f"该列表中不同的质数有 {len(primes)} 个")
+count = find_sum_of_primes(lst)
+print(f"该列表中有 {count} 个质数可以表示为该列表中另外两个质数的和")
