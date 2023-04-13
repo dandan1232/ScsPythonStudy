9b814a0dc464212e1374d332a825b58f8101933b
dandan1232 <345652127@qq.com>
2023-04-13 14:26:17 +0800

    修改

--- ClassStudy/Doc6/doc6_6.py
+++ ClassStudy/Doc6/doc6_6.py
@@ -10,6 +10,7 @@ n = int(input("请输入数字 n："))
 total = 0
 for i in range(1, n + 1):
     num = int(str(a) * i)
+    print("", num)
     total += num
 
 print("S =", total)
--- ClassStudy/Doc6/doc6_8.py
+++ ClassStudy/Doc6/doc6_8.py
@@ -5,3 +5,15 @@
 # @Description :
 # 现有 1 元、2 元和 5 元的钱币若干，如果要用这些钱币（假设足够多）去购买售价n元的商品，请问有多少种不同的组合方式。
 # 请用户输入n，输出所有的组合方案。
+
+n = int(input("请输入售价 n："))
+
+count = 0
+for i in range(0, n // 5 + 1):
+    for j in range(0, (n - 5 * i) // 2 + 1):
+        k = n - 5 * i - 2 * j
+        if k >= 0 and k % 1 == 0:
+            count += 1
+            print("{}元: {}个1元 + {}个2元 + {}个5元".format(n, k, j, i))
+
+print("共有{}种不同的组合方式。".format(count))
