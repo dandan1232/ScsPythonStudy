6b1b86ec94f25aeca686a23e3bb8a005a8f9f026
dandan1232 <345652127@qq.com>
2023-04-27 13:52:41 +0800

    题目框架

---
+++ ClassStudy/Doc8/doc8_5.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/27 13:51
+# @Author  : Lindand
+# @File    : doc8_5.py
+# @Description :
+# 斐波那契数列（Fibonacci sequence），又称黄金分割数列，指0、1、1、2、3、5、8、13、21、34、.......，用数学表达式为F(0)=0，F(1)=1，F(n)=F(n - 1)+F(n - 2)，n ≥ 2。
+# 编写一个递归函数，求解Fibonacci数列的第n项。编写测试程序，从键盘输入n，输出Fibonacci数列第n项的值。并输出斐波那契数列的前10项。
---
+++ ClassStudy/Doc8/doc8_7.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/27 13:52
+# @Author  : Lindand
+# @File    : doc8_7.py
+# @Description :
+# 用递归方法计算下列多项式函数的值：p(x,n)=x-x2+x3-x4+……+(-1)n-1xn  (n>0)
+# 编写测试程序，从键盘输入x和n的值，打印p(x,n)的值。如果x=3.5, n=300呢？
