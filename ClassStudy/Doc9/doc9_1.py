148c5ccba5973680e335083e2ba2180144268227
dandan1232 <345652127@qq.com>
2023-05-04 13:56:09 +0800

    编写程序实现文本文件的复制功能

--- ClassStudy/Doc9/doc9_1.py
+++ ClassStudy/Doc9/doc9_1.py
@@ -5,3 +5,9 @@
 # @Description :
 # 编写程序实现文本文件的复制功能，打开文本文件copy.txt，
 # 在当前路径下新建一个new.txt文件，将copy.txt的内容复制到new.txt文件中，提示拷贝完成。
+
+with open('date/copy.txt', 'r') as src_file:
+    with open('new.txt', 'w') as dst_file:
+        dst_file.write(src_file.read())
+
+print('拷贝完成')
