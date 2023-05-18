3c146c1d7e02384e44ef9e76dd6d111400f5289a
dandan1232 <345652127@qq.com>
2023-05-18 13:47:30 +0800

    定义一个Dog类，实例属性有名字（name）、毛色（color）、体重（weight），方法为叫（bark），通过类名调用该方法时输出wang！Wang！

--- ClassStudy/Doc11/Doc11_2.py
+++ ClassStudy/Doc11/Doc11_2.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/5/18 13:38
+# @Time    : 2023/5/18 13:45
 # @Author  : Lindand
 # @File    : Doc11_2.py
 # @Description :定义一个Dog类，实例属性有名字（name）、毛色（color）、体重（weight），方法为叫（bark），通过类名调用该方法时输出“wang！Wang！”
+class Dog:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
+    def bark(self):
+        print("wang! Wang!")
+
+
+# 创建一个Dog实例
+my_dog = Dog("十七", "白色", 7)
+
+# 调用bark方法
+my_dog.bark()
