b89b94d00444e9b3d477f9f7a5c236e984fedf67
dandan1232 <345652127@qq.com>
2023-06-12 20:46:55 +0800

    编写程序让用户输入自己姓名，输出该姓名字符串的长度。

---
+++ ClassStudy/FinalExam/Doc1/doc1_3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/6/12 20:46
+# @Author  : Lindand
+# @File    : doc1_3.py
+# @Description :编写程序让用户输入自己姓名，输出该姓名字符串的长度。
+
+
+name=input("name:")
+print(len(name))
