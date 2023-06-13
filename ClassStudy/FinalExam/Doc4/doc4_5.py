ae7a808fb8b0bf3649d69d3349a8b0e803c97a09
dandan1232 <345652127@qq.com>
2023-06-13 20:19:02 +0800

    小明想在学校中请一些同学一起做一项问卷调查，请编程帮助小明解决如下问题：

--- ClassStudy/FinalExam/Doc4/doc4_5.py
+++ ClassStudy/FinalExam/Doc4/doc4_5.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/3/30 14:35
+# @Time    : 2023/6/13 20:15
 # @Author  : Lindand
 # @File    : doc4_5.py
 # @Description :
@@ -8,22 +8,10 @@
 # b)	为了实验的客观性先用计算机生成N个1～1000之间的随机整数(N<=1000)；
 # c)	对于其中重复的数字，只保留一个，将其余相同的数字去掉，不同的数对应着不同学生的学号；
 # d)	然后再将这些数从小到大排序，按照排好的顺序去找同学做调查。输出排序的结果。
-#
-
 import random
 
-# 用户输入N
-N = int(input("请输入N："))
+n = int(input("用户输入N；"))
+nums = random.sample(range(0, 1001), n)
 
-# 生成N个1~1000之间的随机整数
-nums = random.sample(range(1, 1001), N)
-
-
-# 去除重复数字
 nums = list(set(nums))
-
-# 排序
-nums.sort()
-
-# 输出结果
-print(nums)
+print(sorted(nums, reverse=False))
